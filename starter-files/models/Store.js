dfe99c81c5bce41ebd518b5a0e4edf4cd6221c89
Jeff Gerlach <jgerlach@tmcdesign.com>
2017-05-22 12:53:03 -0600

    Add in create store actions in controller

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -6,11 +6,24 @@ const storeSchema = new mongoose.Schema({
   name: {
   type: String,
   trim: true,
-
+  required: 'Please enter a store name!',
   },
-  slug
-  description
-  tags
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // can also combine into return next();
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO: make more resiliant so slugs are unique
 });
 
 module.exports = mongoose.model('Store', storeSchema);
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.homePage);
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
