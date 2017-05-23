90bb87c32d0f27718bb85d8af653e5ecf5f8575e
Jeff Gerlach <jgerlach@tmcdesign.com>
2017-05-23 17:00:03 -0600

    Work up to uploading and resizing images

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -12,7 +11,37 @@ exports.addStore = (req, res) => {
 
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save()
-  res.redirect('/');
+  const store = await(new Store(req.body)).save(); // allows us access to the slug
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1) Query database for list of all stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+  // 1) Find store given ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2) confirm they are the owner of the store
+  // TODO
+  // 3) Render out the edit form so user can update their store
+  res.render('editStore', {title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  // set the location data to be a Point
+  req.body.location.type = 'Point';
+  // find and update the store
+  const store = await Store.findOneAndUpdate({_id: req.params.id},
+    req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true,
+  }).exec();
+  req.flash('success',`Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // redirect to the store and tell them it worked
+
+}
